adb8d73716d2012903aeab47f5294585654e3c68
Shivam <sgg86953@gmail.com>
2026-02-16 04:59:22 +0530

    add routing

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,11 +4,15 @@ import express from "express";
 import userRouter from "./user.routes.js";
 import contestRouter from "./contest.routes.js";
 import problemRouter from "./problem.routes.js";
+import submissionRouter from "./submission.routes.js";
+import leaderboardRouter from "./leaderboard.routes.js";
 
 const router = express.Router();
 
 router.use("/v1", userRouter);
 router.use("/v1", contestRouter);
 router.use("/v1", problemRouter);
+router.use("/v1", submissionRouter);
+router.use("/v1", leaderboardRouter);
 
 export default router;
--- backend/src/routes/submission.routes.js
+++ backend/src/routes/submission.routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import * as submissionController from '../controllers/submission.controller.js';
+import {submitController} from '../controllers/submission/submission.controller.js';
 import {submitValidateSchema} from '../validator/submit.validator.js'
 import {validate} from '../middlewares/requestValidator.js'
 import { asyncHandler } from '../utils/response.js';
 const router = express.Router();
 
 router.post('/submit',validate(submitValidateSchema), 
-asyncHandler(submissionController.createSubmission));
+asyncHandler(submitController));
 
 export default router;
